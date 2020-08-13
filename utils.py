a2f9e784cf9cfd69f215d777e7f375ca18823540
Ningxin <Ningxin.Zheng@microsoft.com>
2020-08-13 02:42:58 +0000

    update

--- utils.py
+++ utils.py
@@ -177,10 +177,10 @@ def process_state_dict(state_dict):
     return state_dict
 
 
-# Custom progress bar
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
-TOTAL_BAR_LENGTH = 40.
+# # Custom progress bar
+# _, term_width = os.popen('stty size', 'r').read().split()
+# term_width = int(term_width)
+# TOTAL_BAR_LENGTH = 40.
 last_time = time.time()
 begin_time = last_time
 
